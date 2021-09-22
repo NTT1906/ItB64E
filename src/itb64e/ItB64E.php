6e47dc829c5fb905f3d5d8e47f05336a3dd1e39a
NTT1906 <nttispay@gmail.com>
2021-09-22 08:26:09 +0700

    Adding something

--- src/itb64e/ItB64E.php
+++ src/itb64e/ItB64E.php
@@ -1,10 +1,8 @@
 <?php
-declare(strict_types=1);
-namespace ntt;
+declare(strict_types = 1);
+namespace itb64e;
 
-use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\utils\Config;
 use function array_diff;
@@ -23,7 +21,7 @@ use function strtolower;
 use function unlink;
 use const PATHINFO_EXTENSION;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase implements Listener {
 	/** @var string */
 	public $path;
 	/** @var string */
@@ -33,41 +31,40 @@ class Main extends PluginBase implements Listener{
 	/** @var string[] */
 	public $imageType = ['jpeg', 'png', 'xpm', 'xbm', 'bmp', 'wbmp', 'webp'];
 
-	public function onEnable() : void{
+	public function onEnable() : void {
 		$this->path = $this->getServer()->getDataPath();
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->filePhar = $this->path . "ItB64E/";
-		if(!file_exists($this->filePhar)){
+		if (!file_exists($this->filePhar)) {
 			@mkdir($this->filePhar, 0775, true);
 			$this->getLogger()->info("§aFolder §fItB64E §aGenerated!");
 		}
-		if(!file_exists($this->filePhar . "Image-Input/")){
+		if (!file_exists($this->filePhar . "Image-Input/")) {
 			@mkdir($this->filePhar . "Image-Input/", 0775, true);
 			$this->getLogger()->info("§aFolder §fImage-Input §aGenerated!");
 		}
-		if(!file_exists($this->filePhar . "Data-Output/")){
+		if (!file_exists($this->filePhar . "Data-Output/")) {
 			@mkdir($this->filePhar . "Data-Output/", 0775, true);
 			$this->getLogger()->info("§aFolder §fData-Output §aGenerated!");
 		}
 		$this->getLogger()->info("§aPlugin enabled!");
 		$this->list = array_diff(scandir($this->filePhar . "Image-Input/"), ['.', '..']);
-		foreach($this->list as $f){
+		foreach ($this->list as $f) {
 			$ff = strtolower(pathinfo($this->filePhar . "Image-Input/" . $f, PATHINFO_EXTENSION));
 			$name = str_replace("." . $ff, "", $f);
 			$ImagePhar = $this->filePhar . "Image-Input/" . $f;
-			if(in_array($ff, $this->imageType, true)){
+			if (in_array($ff, $this->imageType, true)) {
 				$data = $this->getDataFromImage($ImagePhar, $ff);
 				$data_output = $this->makeData($data, $name, $ff);
 				$this->getLogger()->info("§f" . $data_output . "§aGenerated!");
-			}else{
+			} else {
 				$this->getLogger()->info("§c$ff is not allowed image type");
 			}
 			unlink($this->filePhar . "Image-Input/" . $f);
 		}
 	}
-	
-	public function getDataFromImage(string $file, string $type){
-		switch($type){
+
+	public function getDataFromImage(string $file, string $type) {
+		switch ($type) {
 			case 'jpeg':
 				$im = \imagecreatefromjpeg($file);
 				break;
@@ -93,13 +90,22 @@ class Main extends PluginBase implements Listener{
 
 		return $im;
 	}
-    
-	public function makeData(string $data, string $name, string $type) : string{
+	
+	public function hexToRgbString($hex) : string {
+		return implode(',', sscanf($hex, '#%02x%02x%02x'));
+	}
+	
+	public function rgbStringToHex(string $rgb) {
+		$rgb = explode(',', $rgb);
+		return $color = sprintf("#%02x%02x%02x", $rgb[0], $rgb[1], $rgb[2]);
+	}
+
+	public function makeData(string $data, string $name, string $type) : string {
 		$bytes = "";
 		$m = getimagesize($this->filePhar . "Image-Input/" . $name . "." . $type)[0];
 		$n = getimagesize($this->filePhar . "Image-Input/" . $name . "." . $type)[1];
-		for($y = 0; $y < $n; ++$y){
-			for($x = 0; $x < $m; ++$x){
+		for ($y = 0; $y < $n; ++$y) {
+			for ($x = 0; $x < $m; ++$x) {
 				$colorat = imagecolorat($data, $x, $y);
 				$a = ((~((int)($colorat >> 24))) << 1) & 0xff;
 				$r = ($colorat >> 16) & 0xff;
@@ -108,7 +114,7 @@ class Main extends PluginBase implements Listener{
 				$bytes .= chr($r) . chr($g) . chr($b) . chr($a);
 			}
 		}
-		file_put_contents($this->filePhar . "Data-Output/" . $name . ".txt" , base64_encode($bytes));
+		file_put_contents($this->filePhar . "Data-Output/" . $name . ".txt", base64_encode($bytes));
 
 		return $name . ".txt";
 	}
